825a358ce318c1cec6668922723978ff80a5d79e
baljinnyam.ye <baljinnyam.ye@cumongol.mn>
2025-03-28 14:22:41 +0800

    add: auth user to sv_master

--- cupp/store_consultant/apps.py
+++ cupp/store_consultant/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class StoreConsultantConfig(AppConfig):
-    name = 'store_consultant'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cupp.store_consultant'
+
+    def ready(self):
+        import cupp.store_consultant.signal
