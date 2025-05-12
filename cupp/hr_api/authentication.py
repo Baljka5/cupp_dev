a0f6cce8e6746d4678f30b55cdf95bc3e2c0f359
baljka baljka <baljinnyambaljka5@gmail.com>
2025-05-12 11:10:06 +0800

    change: hr api apikey

--- cupp/hr_api/authentication.py
+++ cupp/hr_api/authentication.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class APIKeyAuthentication(BaseAuthentication):
     def authenticate(self, request):
-        api_key = request.headers.get('X-API-KEY') or request.GET.get('api_key')
+        api_key = request.META.get('HTTP_X_API_KEY') or request.GET.get('api_key')
         expected_key = getattr(settings, 'CUSTOM_API_KEY', None)
 
         if not api_key or api_key != expected_key:
